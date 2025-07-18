6013fde14173addf0f4c8fbce0f59694c56ec60c
Michael Thaler <michael.thaler@dlr.de>
2025-07-18 16:20:55 +0200

    Updated basic.go.

--- basic.go
+++ basic.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 type Number interface {
 	int | int8 | int16 | int32 | int64 | float32 | float64
 }
@@ -14,6 +16,19 @@ func Mean[N Number](n ...N) float64 {
 }
 
 func Median[N Number](n ...N) N {
+	s := n[:]
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
 	l := len(n)
-	return n[l/2]
+	return s[l/2]
+}
+
+func Variance[N Number](n ...N) float64 {
+	var result float64
+	m := Mean(n...)
+	for _, v := range n {
+		result += (float64(v) - m) * (float64(v) - m)
+	}
+	return result
 }
