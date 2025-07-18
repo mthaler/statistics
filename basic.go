278a4a300c8f5c8f7312480eac79a8f04fec6a8f
Michael Thaler <michael.thaler@dlr.de>
2025-07-18 15:17:17 +0200

    Added basic.go.

---
+++ basic.go
@@ -0,0 +1,14 @@
+package main
+
+type Number interface {
+	int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func Mean[N Number](n ...N) float64 {
+	var sum float64
+	for _, v := range n {
+		sum += float64(v)
+	}
+	sum = sum / float64(len(n))
+	return sum
+}
