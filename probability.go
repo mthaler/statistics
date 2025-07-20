87119c5fec6ed370938c7cc33d39d3ea96610afc
Michael Thaler <michael.thaler@dlr.de>
2025-07-20 09:33:13 +0200

    Updated probability.go again.

--- probability.go
+++ probability.go
@@ -2,6 +2,9 @@ package main
 
 import "math"
 
+/*
+Assumes a constant decay rate, an example is the time until a radioactive particle decays, or the time between clicks of a Geiger counter
+*/
 func Exponential(x, l float64) float64 {
 	return l * math.Exp(-l*x)
 }
